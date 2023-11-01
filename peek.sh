a5e598f8dc4f18f64baf210a072f7b595772643a
alexiapardinas <alexiapardinas@yahoo.es>
2023-11-01 12:12:55 +0100

    Update peek.sh script

--- peek.sh
+++ peek.sh
@@ -1,5 +1,8 @@
 input_file="$1"
 lines="$2"
-head -n $lines $input_file
-echo ...
-tail -n $lines $input_file
+if [[ -z $lines ]]; then lines=3; fi
+
+num_lines=$(wc -l < $input_file)
+
+if [[ $num_lines -le $((2 * lines)) ]]; then echo Full file; cat $input_file
+else head -n $lines $input_file; echo ...; tail -n $lines $input_file; fi
