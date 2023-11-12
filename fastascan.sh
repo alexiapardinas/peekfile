064e4873278ec4d2fb485f81b25a05aae697b630
alexiapardinas <alexiapardinas@yahoo.es>
2023-11-12 13:42:58 +0100

    Updated 12/11

--- fastascan.sh
+++ fastascan.sh
@@ -1,4 +1,4 @@
-# Definition of the optional arguments
+# Define optional arguments
 folder=$1
 nlines=$2
 
@@ -12,6 +12,18 @@ fastafind=$(find $folder -type f -name "*.fa" -or -name "*.fasta")
 # Count how many files
 echo $(echo $fastafind | wc -w)
 
-# Count unique IDs
-cat $fastafind
+# Count unique IDs in each fasta file
+count=0
+for fa in $fastafind; do 
+grep ">" $fa | awk '{print $1}' | sed 's/>//' | sort | uniq | wc -l >> numID
+done
+# Sum all the counted IDs
+awk '{n=n+$1} END {print n}' numID
+# Remove the temporarily created file
+rm numID
 
+for file in $fastafind; do
+echo == ${file##*/} # filename
+if [[ -h $file ]]; then echo Symlink; else echo Not a symlink; fi # symlink or not
+echo Number of sequences: $(grep -c ">" $file) # how many sequences
+done
